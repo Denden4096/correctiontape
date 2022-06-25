c43a162b00d4fcc29b2108fc849fa34ca01b1b57
Denden <denden3840@gmail.com>
2022-06-25 18:02:08 +0900

    ⚡ perf(portal): Remove code for felajs

--- packages/portal/src/index.ts
+++ packages/portal/src/index.ts
@@ -1,11 +1,7 @@
-import { render as styleRender } from "fela-dom"
 import { createApp } from "solid-utils"
 
 import { App } from "./App"
-import { renderer } from "~/style"
 
 document.querySelector("noscript")?.remove()
 
 createApp(App).mount("#wrapper")
-
-styleRender(renderer)
